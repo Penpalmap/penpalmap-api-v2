517c5504daaff0686c65ecf80f221a370d3fca68
Noiret Colin <colin.noiret@gmail.com>
2023-09-05 20:55:10 +0200

    socket update

---
+++ globals.ts
@@ -0,0 +1,1 @@
+export const onlineUsers: Map<string, string> = new Map();
--- socket.ts
+++ socket.ts
@@ -1,15 +1,9 @@
 import { Server } from "http";
 import { Server as SocketServer, Socket } from "socket.io";
 import { Message } from "./sequelize/models/Message";
+import { onlineUsers } from "./globals";
 
 const createSocketServer = (server: Server) => {
-  const io = new SocketServer(server, {
-    pingTimeout: 60000,
-    cors: {
-      origin: `${process.env.FRONTEND_URL}`,
-    },
-  });
-
   const START_TYPING_MESSAGE_EVENT = "START_TYPING_MESSAGE_EVENT";
   const STOP_TYPING_MESSAGE_EVENT = "STOP_TYPING_MESSAGE_EVENT";
   const TYPING_MESSAGE_EVENT = "TYPING_MESSAGE_EVENT";
@@ -17,18 +11,32 @@ const createSocketServer = (server: Server) => {
   const SEND_MESSAGE_EVENT = "SEND_MESSAGE";
   const JOIN_ROOM_EVENT = "JOIN_ROOM";
 
+  const io = new SocketServer(server, {
+    pingTimeout: 60000,
+    cors: {
+      origin: `${process.env.FRONTEND_URL}`,
+    },
+  });
+
   io.on("connection", (socket: Socket) => {
+    socket.on("add-user", (userId: string) => {
+      onlineUsers.set(userId, socket.id);
+    });
+
     socket.on("JOIN_ROOM", (roomId: string) => {
       socket.join(roomId);
     });
 
     socket.on("LEAVE_ROOM", (roomId: string) => {
-      console.log("LEAVE_ROOM_EVENT", roomId);
       socket.leave(roomId);
     });
 
     socket.on(SEND_MESSAGE_EVENT, (message) => {
-      io.in(message.roomId).emit(NEW_CHAT_MESSAGE_EVENT, message);
+      // io.in(message.roomId).emit(NEW_CHAT_MESSAGE_EVENT, message);
+      const receiverSocketId = onlineUsers.get(message.receiverId);
+      if (receiverSocketId) {
+        socket.to(receiverSocketId).emit(NEW_CHAT_MESSAGE_EVENT, message);
+      }
     });
 
     // socket.on(NEW_CHAT_MESSAGE_EVENT, (message: Message) => {

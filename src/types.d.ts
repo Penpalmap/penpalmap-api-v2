6814819b674af51310c8f3be73af32367fb08a9c
RedbeanGit <dubois.julien.mail@gmail.com>
2024-02-04 00:59:17 +0100

    feat: create new upload service using minio
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,11 @@
 import { Request } from "express";
 import { User } from "./user/user.model";
+
+export type MemoryFile = Omit<
+  Express.Multer.File,
+  "destination" | "filename" | "path"
+>;
+
 export interface IGetUserAuthInfoRequest extends Request {
   user: User; // or any other type
 }

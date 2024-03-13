43559fe813926a79599aad7a9a4e76c7658cc5d2
RedbeanGit <dubois.julien.mail@gmail.com>
2024-03-13 08:40:18 +0100

    fix: add missing call to setup
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- src/setup/setup.service.ts
+++ src/setup/setup.service.ts
@@ -3,7 +3,9 @@ import { RoleService } from '../role/role.service';
 
 @Injectable()
 export class SetupService {
-  constructor(private readonly roleService: RoleService) {}
+  constructor(private readonly roleService: RoleService) {
+    this.setup();
+  }
 
   async setup(): Promise<void> {
     await this.roleService.setup();

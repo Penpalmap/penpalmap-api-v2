c9ba5159d8896c20c3ac57c48144645c091321e0
RedbeanGit <dubois.julien.mail@gmail.com>
2024-03-16 10:33:02 +0100

    feat: notify on room event
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

---
+++ src/room/room.gateway.ts
@@ -0,0 +1,28 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import {
+  USER_TYPING_EVENT,
+  UserTypingEventDto,
+} from './dto/user-typing-event.dto';
+import { RoomService } from './room.service';
+import { UseFilters } from '@nestjs/common';
+import { SocketExceptionsFilter } from '../shared/socket/socket-exceptions.filter';
+import { Socket } from 'socket.io';
+
+@WebSocketGateway()
+@UseFilters(SocketExceptionsFilter)
+export class RoomGateway {
+  constructor(private readonly roomService: RoomService) {}
+
+  @SubscribeMessage(USER_TYPING_EVENT)
+  async handleUserTyping(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() message: UserTypingEventDto,
+  ): Promise<void> {
+    this.roomService.notifyUserTyping(client, message);
+  }
+}
--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -4,11 +4,13 @@ import { RoomController } from './room.controller';
 import { UserModule } from '../user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Room from './room.model';
+import { SocketModule } from '../socket/socket.module';
+import { RoomGateway } from './room.gateway';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Room]), UserModule],
+  imports: [TypeOrmModule.forFeature([Room]), UserModule, SocketModule],
   exports: [RoomService],
-  providers: [RoomService],
+  providers: [RoomService, RoomGateway],
   controllers: [RoomController],
 })
 export class RoomModule {}

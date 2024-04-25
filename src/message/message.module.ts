10ed8ea337aa5797e921b651b977bf7f4820127d
Colingue <colin.noiret@gmail.com>
2024-04-25 20:37:36 +0200

    Add ScheduleModule for cron job in message module

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -7,9 +7,11 @@ import { UserModule } from '../user/user.module';
 import { RoomModule } from '../room/room.module';
 import { SocketModule } from '../socket/socket.module';
 import { MailjetModule } from '../mailjet/mailjet.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     TypeOrmModule.forFeature([Message]),
     UserModule,
     RoomModule,
